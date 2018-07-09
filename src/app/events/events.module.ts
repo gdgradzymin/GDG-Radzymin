ac6e9637f3f73506fafd4f19c5ce4486d281b535
Sebastian Denis <sebastian.denis@outlook.com>
2018-07-09 11:37:09 +0200

    0.1.0

---
+++ src/app/events/events.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EventsRoutingModule } from './events-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EventsRoutingModule
+  ],
+  declarations: []
+})
+export class EventsModule { }
---
+++ src/app/team/team.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeamRoutingModule } from './team-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TeamRoutingModule
+  ],
+  declarations: []
+})
+export class TeamModule { }
