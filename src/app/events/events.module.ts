e9db9e6c67c5783e8fc6c766e666dd5259fbe742
Sebastian Denis <sebastian.denis@outlook.com>
2019-04-20 23:28:35 +0200

    ngrx setup for events - start

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EventsRoutingModule } from './events-routing.module';
-import { EventsComponent } from './containers/events/events.component';
-import { SharedModule } from '../shared/shared.module';
-import { EventCardComponent } from './components/event-card/event-card.component';
-import { MaterialModule } from '../material/material.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { EventsRoutingModule } from "./events-routing.module";
+import { EventsComponent } from "./containers/events/events.component";
+import { SharedModule } from "../shared/shared.module";
+import { EventCardComponent } from "./components/event-card/event-card.component";
+import { MaterialModule } from "../material/material.module";
+import { StoreModule } from "@ngrx/store";
+import { reducers } from "../reducers/index";
+import * as fromEvents from "../reducers/events.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { EventsEffects } from "../effects/events.effects";
 
 @NgModule({
   imports: [
     CommonModule,
     EventsRoutingModule,
     SharedModule,
-    MaterialModule
+    StoreModule.forFeature("eventsModule", reducers),
+    MaterialModule,
+    StoreModule.forFeature("events", fromEvents.reducer),
+    EffectsModule.forFeature([EventsEffects])
   ],
   exports: [
     EventsComponent, EventCardComponent
